51a71ed043d41b0f88149100a8856b634d7d5b7c
Tloz <scrimet@hotmail.fr>
2014-01-22 11:20:12 +0100

    .

--- main.cpp
+++ main.cpp
@@ -8,21 +8,6 @@
 
 int main()
 {
-    QVector<QString> *output = new QVector<QString>();
-    QVector<QChar> *miss = new QVector<QChar>();
-    miss->append('T');
-    miss->append('X');
-    lang l = FR;
-    output = positionSort("__A__", 0);
-    qDebug() << endl << "partie 1";
-    for(int i = 0; i < output->size(); i++)
-        qDebug() << output->at(i);
-
-    output = doubleSort("__A__", output);
-    qDebug() << endl << "partie 3";
-    for(int i = 0; i < output->size(); i++)
-        qDebug() << output->at(i);
-
-
+    test();
     return 0;
 }
