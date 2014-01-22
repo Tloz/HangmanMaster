2cd9517bfbd6e5918e9089064afde8b4d1a25f5d
Tloz <scrimet@hotmail.fr>
2014-01-22 11:05:40 +0100

    minor change on debug messages

--- test.cpp
+++ test.cpp
@@ -3,23 +3,22 @@
 
 void test()
 {
-    const QString mysteryWord("__A__");
+    const QString mysteryWord("__A_E");
     QVector<QString> dictionary (openDict(mysteryWord.size(), FR));
     QVector<QChar> *miss = new QVector<QChar>();
     miss->append('T');
     miss->append('X');
+    miss->append('I');
     qDebug() << mysteryWord << endl << dictionary << endl << *miss << endl;
     QVector<QString> *output = new QVector<QString>();
 
-
+    qDebug() << "partie 1",
     output = positionSort(mysteryWord, dictionary);
-    qDebug() << "partie 1" << endl << *output << endl;
+    qDebug() << *output << endl;
 
-
-    /*
+    qDebug() << "partie 2";
     output = missingSort(output, miss);
-    qDebug() << "partie 2" << endl << *output << endl;
-    */
+    qDebug() << *output << endl;
 
     /*
     output = doubleSort(mysteryWord, output);
