80f08641edc8d7b0025a971968dfa539ad3767dd
Tloz <scrimet@hotmail.fr>
2014-01-22 01:00:37 +0100

    updated with test routines

--- main.cpp
+++ main.cpp
@@ -1,25 +1,28 @@
 #include <QDebug>
 #include <QString>
 
+#include "test.h"
+
+
 #include "core/algorithm.h"
 
 int main()
 {
     QVector<QString> *output = new QVector<QString>();
     QVector<QChar> *miss = new QVector<QChar>();
-    miss->append('C');
+    miss->append('T');
+    miss->append('X');
+    lang l = FR;
     output = positionSort("__A__", 0);
-
-    qDebug() << "partie 1";
+    qDebug() << endl << "partie 1";
     for(int i = 0; i < output->size(); i++)
         qDebug() << output->at(i);
 
-    output = missingSort(output, miss);
-
-    qDebug() << endl << "partie 2";
+    output = doubleSort("__A__", output);
+    qDebug() << endl << "partie 3";
     for(int i = 0; i < output->size(); i++)
         qDebug() << output->at(i);
 
-    qDebug() << endl << "THE END";
+
     return 0;
 }
