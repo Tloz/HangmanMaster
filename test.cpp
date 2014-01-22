ad6d907e80ef8a49233d91eaedd9cce2c8bdcbb8
Tloz <scrimet@hotmail.fr>
2014-01-22 10:00:41 +0100

    create a bunch if tests that should work fine if the algo is correct

--- test.cpp
+++ test.cpp
@@ -3,32 +3,32 @@
 
 void test()
 {
-
-    QVector<QString> *output = new QVector<QString>();
+    const QString mysteryWord("__A__");
+    QVector<QString> dictionary (openDict(mysteryWord.size(), FR));
     QVector<QChar> *miss = new QVector<QChar>();
     miss->append('T');
     miss->append('X');
-    lang l = FR;
-    //solveWord(QString("__A__"), 0, miss);
-    output = positionSort("__A__", 0);
+    QVector<QString> *output = new QVector<QString>();
 
+
+    output = positionSort(mysteryWord, dictionary);
     qDebug() << "partie 1";
     for(int i = 0; i < output->size(); i++)
         qDebug() << output->at(i);
 
+    /*
     output = missingSort(output, miss);
-/*
     qDebug() << endl << "partie 2";
     for(int i = 0; i < output->size(); i++)
         qDebug() << output->at(i);
     qDebug() << endl << endl;
     */
 
-    output = doubleSort("__A__", output);
-
+    /*
+    output = doubleSort(mysteryWord, output);
     qDebug() << endl << "partie 3";
     for(int i = 0; i < output->size(); i++)
         qDebug() << output->at(i);
-
+    */
     qDebug() << endl << "THE END";
 }
